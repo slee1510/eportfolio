f1e1c2688e02e3836f79db4c3c706c72da58eb54
sydney <sydneylee1510@gmail.com>
2025-06-26 13:00:39 +0800

    header functional

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+// Load Header
+fetch("header.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("header-placeholder").innerHTML = data;
 
+    highlightCurrentPage();
+  });
+
+// Load Footer
+fetch("footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer-placeholder").innerHTML = data;
+  });
+
+  function highlightCurrentPage() {
+    const currentPath = window.location.pathname.split("/").pop();
+    const navLinks = document.querySelectorAll(".nav-right a");
+
+    navLinks.forEach(link => {
+        if (link.getAttribute("href") === currentPath) {
+            link.classList.add("active");
+        }
+    });
+  }
